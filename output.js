53ae371e009a2fc260f56c0c730775f584068df2
Daniel Jabry <daniel.jabry@rezonence.com>
2019-02-23 12:26:16 +0000

    Configure sls outputs

---
+++ output.js
@@ -0,0 +1,3 @@
+exports.handler = (output) => {
+  console.log("Received output:", output)
+};
--- src/app/aws.service.ts
+++ src/app/aws.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import {CognitoIdentityAwsServiceFactory} from '@djabry/aws-factory/examples/cognito.identity.aws.service.factory';
 import { CognitoIdentityCredentials } from 'aws-sdk/lib/credentials/cognito_identity_credentials';
+import * as stack from '../../.serverless/stack.json';
+import {config} from 'aws-sdk/global';
+
+config.region = stack.region;
 
 @Injectable({
   providedIn: 'root'
