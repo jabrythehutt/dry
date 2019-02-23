17a2e7e1f7ab40ea7cd99e10072117428b6b7dc9
Daniel Jabry <daniel.jabry@rezonence.com>
2019-02-23 18:38:54 +0000

    Implement conditional save

--- src/app/note/note-dao.service.ts
+++ src/app/note/note-dao.service.ts
@@ -3,6 +3,8 @@ import {Entity} from '../data/entity';
 import {Note} from './note';
 import {AwsService} from '../aws.service';
 import {UserEntityDao} from '../data/user.entity.dao';
+import {User} from '../user/user';
+import {HashService} from '../data/hash.service';
 
 const indexName = 'userId-index';
 
@@ -11,7 +13,37 @@ const indexName = 'userId-index';
 })
 export class NoteDaoService extends UserEntityDao<Entity, Note> {
 
-  constructor(awsService: AwsService) {
+  constructor(awsService: AwsService,
+              private hashService: HashService) {
     super(awsService.dynamodb(), 'bar', indexName);
+  }
+
+  async saveNote(note: Note): Promise<void> {
+    const db = await this.dbPromise;
+    // Don't save the same not twice
+    const idExpressionAttributes = this.toExpressionAttributes('id', note.id);
+    try {
+      await db.put({
+        TableName: this.tableName,
+        ExpressionAttributeNames: {
+          [idExpressionAttributes.keyName]: idExpressionAttributes.key
+        },
+        Item: note,
+        ConditionExpression: `attribute_not_exists(${idExpressionAttributes.keyName})`
+      }).promise();
+    } catch (err) {
+      if (err.code !== 'ConditionalCheckFailedException') {
+        throw err;
+      }
+    }
+  }
+
+  createNote(user: User, text: string ): Note {
+    const id = this.hashService.getHash(user.id + text);
+    return {
+      userId: user.id,
+      text,
+      id
+    };
   }
 }
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../user/user';
 import {UserDaoService} from '../user/user-dao.service';
+import {NoteDaoService} from '../note/note-dao.service';
+import {Note} from '../note/note';
 
 @Component({
   selector: 'app-upload',
@@ -14,10 +16,16 @@ export class UploadComponent implements OnInit {
   selectedUser: User;
   text: string;
 
-  constructor(private usersDao: UserDaoService) { }
+  constructor(private usersDao: UserDaoService,
+              private notesDao: NoteDaoService) { }
 
   ngOnInit() {
    this.usersPromise = this.usersDao.list();
+  }
+
+  async saveNote(): Promise<void> {
+    const note = await this.notesDao.createNote(this.selectedUser, this.text);
+    await this.notesDao.saveNote(note);
   }
 
   goClicked() {
