f025b969614a02f2dc80b729b5fde261fd63f553
Daniel Jabry <daniel.jabry@rezonence.com>
2019-02-23 01:35:50 +0000

    Configure aws service

--- src/app/aws.service.ts
+++ src/app/aws.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {CognitoIdentityAwsServiceFactory} from '@djabry/aws-factory/examples/cognito.identity.aws.service.factory';
+import { CognitoIdentityCredentials } from 'aws-sdk/lib/credentials/cognito_identity_credentials';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AwsService {
+export class AwsService extends CognitoIdentityAwsServiceFactory {
 
-  constructor() { }
+  constructor() {
+    super(new CognitoIdentityCredentials({IdentityPoolId: ''}));
+  }
 }
