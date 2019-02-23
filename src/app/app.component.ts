21f2f83ee1d6de74596c29cc45a6e5a71339cd0f
Daniel Jabry <daniel.jabry@rezonence.com>
2019-02-23 23:33:21 +0000

    Correct sizing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {fadeAnimation} from '../animations';
 
 @Component({
   selector: 'app-root',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [fadeAnimation]
