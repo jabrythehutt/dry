4335b208795777f78852dc5a18a1b45ed47bd3c3
Daniel Jabry <daniel.jabry@rezonence.com>
2019-02-23 23:30:40 +0000

    Add fade animations to route transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {fadeAnimation} from '../animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeAnimation]
 })
 export class AppComponent {
   title = 'DRY';
